707d0c9dec45834059ddd25e64d0a2ca45c0446f
SachinSilva99 <sachinmanathunga@gmail.com>
2023-09-12 15:38:37 +0530

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>hello</h1>,
+    },
+    {
+        path: "/hi",
+        element: <h1>hi</h1>,
+    },
+]);
 function App() {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            POS
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#customers">
-                  Customers
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#orders">
-                  Orders
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#items">
-                  Items
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#placeOrder">
-                  Place Order
-                </a>
-              </li>
-            </ul>
-            <div className="avatar-container nav-item">
-              <div className="avatar">
-                <img src="assets/img/avatar.png" alt="img" />
-              </div>
-              <h3>Sachin</h3>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
 export default App;
